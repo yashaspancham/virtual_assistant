8ece164173a2dc5b8126b49c1c6dc9a2c5210c25
Yashas Pancham <yashaspancham@gmail.com>
2023-04-26 12:47:35 +0530

    v1.py

--- processes.py
+++ processes.py
@@ -2,6 +2,7 @@ import say
 import name
 import jokes
 import googfind
+import stock_data
 
 def process(said):
     print("Processing the string input")
@@ -9,3 +10,4 @@ def process(said):
     #name.nname(said)
     jokes.jokes(said)
     googfind.googfind(said)
+    stock_data.stock_data(said)
